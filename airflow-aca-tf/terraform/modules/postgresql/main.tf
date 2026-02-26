34fcbd7fa78cf08e51214519a53bfad95b30d381
oleksandr merzlikin <aleksander.merzlikin@nixsolutions.com>
2026-02-26 01:12:54 +0100

    remove Ha

--- airflow-aca-tf/terraform/modules/postgresql/main.tf
+++ airflow-aca-tf/terraform/modules/postgresql/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
   name                = "${var.name}-pg-server"
   resource_group_name = var.resource_group_name
   location            = var.location
-  version             = "16"
+  version             = "17"
 
   administrator_login    = "airflow"
   administrator_password = var.admin_password
@@ -13,12 +13,12 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
 
-  dynamic "high_availability" {
-    for_each = var.deployment_mode == "production" ? [1] : []
-    content {
-      mode = "ZoneRedundant"
-    }
-  }
+#  dynamic "high_availability" {
+#    for_each = var.deployment_mode == "production" ? [1] : []
+#    content {
+#      mode = "ZoneRedundant"
+#    }
+#  }
 
   tags = var.tags
 }
